54947d2b8b4204129edb29c895fe4db8d5106435
Karim-Moharm <karimmoharm222@gmail.com>
2023-10-05 15:41:00 -0700

    fixing code style

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,16 +5,18 @@ from fabric.api import local, run
 from datetime import datetime
 import os
 
-def do_pack():
-	"""generates a .tgz archive from the contents of the web_static folder
-	"""
-	formatted_dt = datetime.now().strftime("%Y%m%d%H%M%S")
-	archieve_name = "web_static_{}.tgz".format(formatted_dt)
 
-	try:
-		local("mkdir -p versions")
-		local("tar -czvf versions/{} versions".format(archieve_name))
-		file_size = os.path.getsize("versions/{}".format(archieve_name))
-		print("web_static packed: versions/{} -> {}Bytes".format(archieve_name, file_size))		
-	except:
-		return None	
+def do_pack():
+    """generates a .tgz archive from the contents of the web_static folder
+    """
+    formatted_dt = datetime.now().strftime("%Y%m%d%H%M%S")
+    archieve_name = "web_static_{}.tgz".format(formatted_dt)
+
+    try:
+        local("mkdir -p versions")
+        local("tar -czvf versions/{} versions".format(archieve_name))
+        file_size = os.path.getsize("versions/{}".format(archieve_name))
+        print("web_static packed: versions/{} -> {}Bytes".format(archieve_name,
+                                                                 file_size))
+    except Exception:
+        return None
