9015b7d60d8aca89d434a5db1bd951e65aa5b684
Karim-Moharm <karimmoharm222@gmail.com>
2023-10-05 19:44:06 -0700

    update the deploy file

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,28 @@ import os
 from fabric.api import local, put, run, env
 
 env.hosts = ['100.25.23.34', '52.87.216.135']
-env.user = 'ubuntu'
+# env.user = 'ubuntu'
+
+
+def do_pack():
+    """generates a .tgz archive from the contents of the web_static folder
+    """
+    formatted_dt = datetime.now().strftime("%Y%m%d%H%M%S")
+    archieve_name = "web_static_{}.tgz".format(formatted_dt)
+
+    try:
+        local("mkdir -p versions")
+        result = local("tar -czvf versions/{} web_static".format(
+                        archieve_name))
+        file_size = os.path.getsize("versions/{}".format(archieve_name))
+        print("web_static packed: versions/{} -> {}Bytes".format(archieve_name,
+                                                                 file_size))
+        if result.succeeded:
+            return f"versions/{archieve_name}"
+        else:
+            return None
+    except Exception:
+        return None
 
 
 def do_deploy(archive_path):
