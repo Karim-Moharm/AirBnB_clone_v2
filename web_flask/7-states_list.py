290ee78e3f3edea378102979fa8a6c8459de27a7
Karim-Moharm <karimmoharm222@gmail.com>
2023-10-21 20:23:30 +0300

    display state data

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown():
+    """close the session after fetching the data
+    """
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def display_states_data():
+    """display List of states
+    """
+    state_values = storage.all(State).values()
+    return render_template('7-states_list.html', state_values=state_values, title='HBNB')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
