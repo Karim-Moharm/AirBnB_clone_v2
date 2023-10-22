eb163f416c3314a64c21e01a3cdcfcf167da3dcc
Karim-Moharm <karimmoharm222@gmail.com>
2023-10-22 22:22:56 +0300

    update path

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """script that starts a Flask web application
 """
-from flask import Flask, render_template
-from models import storage
 from models.state import State
+from models import storage
+from flask import Flask, render_template
+import sys
+sys.path.append('..')
 
 app = Flask(__name__)
 
